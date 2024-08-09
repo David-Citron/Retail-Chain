bac6e6e0fda06f11b041396c636be81b757e3ff6
David Žižlavský <davidcitron2@seznam.cz>
2024-08-09 18:14:39 +0200

    Test of kick callback with correct initialization

--- Assets/Scripts/SteamManager/SteamLobby.cs
+++ Assets/Scripts/SteamManager/SteamLobby.cs
@@ -13,6 +13,7 @@ public class SteamLobby : MonoBehaviour
     protected Callback<LobbyCreated_t> lobbyCreated;
     protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
     protected Callback<LobbyEnter_t> lobbyEntered;
+    protected Callback<LobbyKicked_t> lobbyKicked;
 
     private const string HostAdressKey = "HostAdressKey";
 
@@ -32,6 +33,7 @@ public class SteamLobby : MonoBehaviour
         lobbyCreated = Callback<LobbyCreated_t>.Create(onLobbyCreated);
         gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
         lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+        lobbyKicked = Callback<LobbyKicked_t>.Create(OnLobbyExited);
     }
 
     public void HostLobby()
