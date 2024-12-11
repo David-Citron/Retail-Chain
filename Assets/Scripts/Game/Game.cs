2656bd78cd6cfe3ba37ca31d96e13f04b3f55c92
Michal Hlúch <m.hluch@seznam.cz>
2024-12-11 16:57:30 +0100

    removed gamestate

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -5,7 +5,6 @@ public class Game : MonoBehaviour
 {
 
     private static Game instance;
-    private GameState gameState;
 
     void Start()
     {
@@ -16,20 +15,6 @@ public class Game : MonoBehaviour
     void Update()
     {
         
-    }
-
-    public void setGameState(GameState newState)
-    {
-        if (gameState == newState) return;
-
-        gameState = newState;
-
-        switch(gameState)
-        {
-            case GameState.Ending:
-                NetworkManager.singleton.ServerChangeScene("MainMenu");
-                break;
-        }
     }
 
     public static Optional<Game> Instance() => Optional<Game>.Of(instance);
