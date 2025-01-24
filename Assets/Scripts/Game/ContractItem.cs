1ed0b64a34f76bc12e8b4758cd6388c867f4bbd3
David Žižlavský <davidcitron2@seznam.cz>
2025-01-24 15:48:25 +0100

    Contract serialization changes

--- Assets/Scripts/Game/ContractItem.cs
+++ Assets/Scripts/Game/ContractItem.cs
@@ -1,3 +1,5 @@
+using Mirror;
+
 [System.Serializable]
 public class ContractItem
 {
@@ -31,5 +33,20 @@ public class ContractItem
         quantityRemaining--;
         if (quantityRemaining > 0) return;
         fulfilled = true;
+    }
+}
+
+public static class ContractItemReaderWriter
+{
+    public static void WriteContractItem(this NetworkWriter writer, ContractItem contractItem)
+    {
+        writer.WriteByte((byte)contractItem.itemType);
+        writer.WriteInt(contractItem.quantity);
+        writer.WriteInt(contractItem.price);
+    }
+
+    public static ContractItem ReadContractItem (this NetworkReader reader)
+    {
+        return new ContractItem((ItemType)reader.ReadByte(), reader.ReadInt(), reader.ReadInt());
     }
 }
