9a4b4668deab8b2fd450c65c157504c17f698d6c
Michal Hlúch <m.hluch@seznam.cz>
2025-03-20 08:49:47 +0100

    Removed comments

--- Assets/Scripts/Game/Shop/Customer/Customer.cs
+++ Assets/Scripts/Game/Shop/Customer/Customer.cs
@@ -258,10 +258,11 @@ public class Customer : MonoBehaviour
         Leave();
         CustomerManager.instance.UpdateQueue();
 
-        bool cheaperThanRecommended = bill.price <= PriceSystem.CalculateRecommendedPrice(ItemManager.GetItemData(bill.itemType).sellPrice);
+        int basePrice = ItemManager.GetItemData(bill.itemType).sellPrice<
+        bool cheaperThanRecommended = bill.price <= PriceSystem.CalculateRecommendedPrice();
 
-        float rating = cheaperThanRecommended ? 0.15f : -0.1f;//If the price is cheaper than recommended increase rating.
-        if(stepsCount >= 2) rating -= stepsCount * .05f; //If the steps count is >= 2 than decrease rating
+        float rating = cheaperThanRecommended ? 0.15f : -0.1f;    //If the price is cheaper than recommended increase rating.
+        if(stepsCount >= 2) rating -= stepsCount * .05f;          //If the steps count is >= 2 than decrease rating
 
         ShopRating.instance.IncreaseRating(rating);
 
